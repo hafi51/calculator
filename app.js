e4f1889319b690181cc2c2f3c4c497ab5ca6e763
hafi51 <mmuazazhar@gmail.com>
2020-07-07 22:23:06 +0500

    error showed in alerts

--- app.js
+++ app.js
@@ -10,8 +10,14 @@ function clearInput(){
     result.innerHTML = "0"
 }
 function getResult(){
+    if(input.innerHTML == "undefined"){
+        result.innerHTML = "0"
+    }
     result.innerHTML = eval(input.innerHTML)
 }
 function returned(){
     input.innerHTML = input.innerHTML.slice(0,-1)
+}
+window.onerror = function(){
+    alert("Error Occured. Check Input.")
 }
