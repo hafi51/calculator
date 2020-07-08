32ff78cc914f2744c145e03fcf2ede2dceb348b3
hafi51 <mmuazazhar@gmail.com>
2020-07-08 14:18:32 +0500

    added sound effect

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 var result = document.getElementById("result");
 result.innerHTML = "0";
 var input = document.getElementById("input");
-function getButton(button){
+
+function getButton(button) {
     input.innerHTML += button;
     result.innerHTML = ""
 }
-function clearInput(){
+
+function clearInput() {
     input.innerHTML = ""
     result.innerHTML = "0"
 }
-function getResult(){
-    if(input.innerHTML == "undefined"){
+
+function getResult() {
+    if (input.innerHTML == "undefined") {
         result.innerHTML = "0"
     }
     result.innerHTML = eval(input.innerHTML)
 }
-function returned(){
-    input.innerHTML = input.innerHTML.slice(0,-1)
+
+function returned() {
+    input.innerHTML = input.innerHTML.slice(0, -1)
 }
-window.onerror = function(){
+window.onerror = function () {
     alert("Error Occured. Check Input.")
+}
+
+var click = new Audio();
+click.src = "click.mp3"
+function playAudio(){
+    click.play()
 }
